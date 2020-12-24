e93494dedd5408d12da40eb7b9178a084a8b2764
Katherine Santiago <santiago.kat@gmail.com>
2020-12-24 00:44:20 -0500

    made minor style changes

--- src/SelectSize.js
+++ src/SelectSize.js
@@ -11,10 +11,10 @@ const useClickOutside = (handler) => {
         handler()
       }
     }
-    document.addEventListener("mousedown", mouseDwnHandler)
+    document.addEventListener('mousedown', mouseDwnHandler)
     // clean up the listener
     return () => {
-      document.removeEventListener("mousedown", mouseDwnHandler)
+      document.removeEventListener('mousedown', mouseDwnHandler)
     }
   })
 
@@ -22,60 +22,64 @@ const useClickOutside = (handler) => {
 }
 
 const SelectSize = (props) => {
-  const {selectedSize, sizes, showSizes, dropDown, setShowSizes, selectSize, sizeMenuRef} = props
-  
+  const {
+    selectedSize,
+    sizes,
+    showSizes,
+    dropDown,
+    setShowSizes,
+    selectSize,
+    sizeMenuRef,
+  } = props
 
   let domNode = useClickOutside(() => {
     setShowSizes(false)
   })
 
   return (
-    <div ref={domNode} className="select-wrapper">
-      <div 
-        className="select-box--box">
-
+    <div ref={domNode} className='select-wrapper'>
+      <div className='select-box--box'>
         {/* label and arrow for selecting size */}
-        <div
-          className="select-box--container">
-            <div className="select-box-label">
-              <div 
-                onClick={dropDown}    
-                className="select-box--selected-item">
-                {selectedSize.value || "size"}
-              </div>
-              <div 
-                className="select-box--arrow"
-                onClick={dropDown}    
-              >
-                <span 
-                  className={`${showSizes ? 'select-box--arrow-up' : 'select-box--arrow-down'}`}>
-                </span>
-              </div>
+        <div className='select-box--container'>
+          <div className='select-box-label'>
+            <div onClick={dropDown} className='select-box--selected-item'>
+              {selectedSize.value || 'size'}
             </div>
+            <div className='select-box--arrow' onClick={dropDown}>
+              <span
+                className={`${
+                  showSizes ? 'select-box--arrow-up' : 'select-box--arrow-down'
+                }`}
+              ></span>
+            </div>
+          </div>
 
           {/* options for size */}
-          <div 
-            className="select-box--items"
-            style={{display: showSizes ? 'block' : 'none'}}>
-            {
-              sizes.map(size => <div 
+          <div
+            className='select-box--items'
+            style={{
+              background: 'white',
+              display: showSizes ? 'block' : 'none',
+            }}
+          >
+            {sizes.map((size) => (
+              <div
                 key={size.id}
                 onClick={() => selectSize(size)}
                 className={selectedSize.value === size.value ? 'selected' : ''}
               >
                 {size.value}
-              </div>)
-            }
+              </div>
+            ))}
           </div>
-
         </div>
       </div>
 
       {/* hidden input to keep track of changes to state */}
-      <input 
-        type="hidden" 
+      <input
+        type='hidden'
         value={selectedSize}
-        name="size"
+        name='size'
         onChange={() => selectSize(selectedSize)}
       />
     </div>
